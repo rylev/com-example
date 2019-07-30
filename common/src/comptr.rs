b06c1c23735e1b6c88bf81fd130018c6cee8c6d3
Hadrian Wei Heng Lim <t-halim@microsoft.com>
2019-07-30 18:53:11 +0100

    Change comptr to convert raw pointer to reference on create

--- common/src/comptr.rs
+++ common/src/comptr.rs
@@ -7,45 +7,50 @@ use std::os::raw::c_void;
 use std::ops::Deref;
 use std::ops::DerefMut;
 
-pub struct ComPtr<T> {
+pub struct ComPtr<'a, T:'a> {
     // Interface pointer for interface T as a box.
-    raw_ptr: *mut T
+    raw_ptr: &'a mut T
 }
 
-impl<T> Deref for ComPtr<T> {
+impl<T> Deref for ComPtr<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        unsafe {&*(self.raw_ptr)}
+        self.raw_ptr
     }
 }
 
-impl<T> DerefMut for ComPtr<T> {
+impl<T> DerefMut for ComPtr<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe {&mut *(self.raw_ptr)}
+        self.raw_ptr
     }
 }
 
-impl<T> ComPtr<T> {
-    pub unsafe fn create_instance(rclsid: REFCLSID, pUnkOuter: *mut IUnknown, dwClsContext: DWORD, riid: REFIID) -> Self {
+impl<T> ComPtr<'_, T> {
+    pub fn create_instance(rclsid: REFCLSID, pUnkOuter: *mut IUnknown, dwClsContext: DWORD, riid: REFIID) -> Self {
         let mut p = std::ptr::null_mut::<c_void>();
-        // let hr = CoCreateInstance(rclsid, std::ptr::null_mut() as *mut IUnknown, dwClsContext, riid, &mut p);
 
-        let hr = CoGetClassObject(rclsid, CLSCTX_INPROC_SERVER, std::ptr::null_mut::<c_void>(), riid, &mut p as *mut LPVOID);
-        if failed(hr) {
-            panic!("Failed to create instance!");
-        }
-        let raw_ptr = p as *mut T;
+        unsafe {
+            // let hr = CoCreateInstance(rclsid, std::ptr::null_mut() as *mut IUnknown, dwClsContext, riid, &mut p);
+            let hr = CoGetClassObject(rclsid, CLSCTX_INPROC_SERVER, std::ptr::null_mut::<c_void>(), riid, &mut p as *mut LPVOID);
+            if failed(hr) {
+                panic!("Failed to create instance!");
+            }
+        
+            let raw_ptr = &mut *(p as *mut T);
 
-        ComPtr {
-            raw_ptr
+            ComPtr {
+                raw_ptr
+            }
         }
+
+        
     }
 }
 
-impl<T> Drop for ComPtr<T> {
+impl<T> Drop for ComPtr<'_, T> {
     fn drop(&mut self) {
-        unsafe {(*(self.raw_ptr as *mut IUnknown)).release();}
+        unsafe {(*(self.raw_ptr as *mut T as *mut IUnknown)).release();}
     }
 }
 
